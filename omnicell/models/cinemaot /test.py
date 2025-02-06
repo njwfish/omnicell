6ff1ccc958738961789129e15fd365677ca294bc
Mohcene Maher Bouceneche <33347924+MohcenMaher@users.noreply.github.com>
2025-02-06 08:50:20 -0500

    Update test.py

--- omnicell/models/cinemaot /test.py
+++ omnicell/models/cinemaot /test.py
@@ -5,7 +5,7 @@ from omnicell.models.cinemaot.predictor import CinemaOTPredictorWrapper
 adata = pt.dt.cinemaot_example()
 
 config = {
-    "pert_key": "perturbation",
+    "pert_key": "pert",
     "control": "ctrl",
     "thres": 0.5,
     "preweight_label": "cell_type0528"
